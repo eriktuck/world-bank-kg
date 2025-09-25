75f472b2572c82fe52c661ac6b6d1d42a0a4b641
erik <eriktuck@gmail.com>
2025-09-25 11:29:59 -0600

    Move EntityRuler to pipeline.

--- src/ner.py
+++ src/ner.py
@@ -3,23 +3,19 @@ import spacy
 from spacy.lang.en.stop_words import STOP_WORDS
 from spacy.pipeline import EntityRuler
 
+
 """
 Requirements
 - extract named entities of desired type
 """
 
 class EntityExtractor:
-    def __init__(self, model: str = "en_core_web_sm"):
-        self.nlp = spacy.load(model)
+    def __init__(self):
+        pass
         
-    def extract(self, text: str, acronyms: Dict) -> List[Tuple[str, str, Tuple[int, int]]]:
-        ruler: EntityRuler = self.nlp.add_pipe("entity_ruler", before="ner")
-        patterns = [
-            {"label": "ACRONYM", "pattern": [{"LOWER": abbr.lower()}], "id": abbr}
-            for abbr in acronyms
-        ]
-        ruler.add_patterns(patterns)
-
-        doc = self.nlp(text)
+    def extract(self, nlp, text: str) -> List[Tuple[str, str, Tuple[int, int]]]:
+        doc = nlp(text)
 
         return [(ent.text, ent.label_, (ent.start_char, ent.end_char)) for ent in doc.ents]
+    
+
